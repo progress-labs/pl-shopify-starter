027fe3a7b72cdd720fd00961a0ea7ad5281e665b
patrickbjohnson <pbj@progresslabs.co>
2026-07-28 15:55:30 -0400

    build: gate build/deploy on tsc, lint TypeScript

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,11 @@
 import js from '@eslint/js'
 import globals from 'globals'
+import tseslint from 'typescript-eslint'
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   js.configs.recommended,
+  ...tseslint.configs.recommended,
   {
     languageOptions: {
       globals: {
